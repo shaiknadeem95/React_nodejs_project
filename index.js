02dcc4aceab78edb5925a0a343e402c0f82ed94b
Nadeem Shaik <shaiknadeem98@gmail.com>
2020-11-14 15:03:30 +0530

    first commit

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express=require('express')
 const app=express()
 
 app.get('/',(req,res)=>{
-   // console.log("hello ")
+    console.log("hello ")
+    console.log("never say never")
     res.send({hi:"good to see you"})
 })
 
