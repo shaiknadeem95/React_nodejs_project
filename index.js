ed460452a148739a8b2452057e8df4ab60a19ea5
Nadeem Shaik <shaiknadeem98@gmail.com>
2020-11-14 15:33:53 +0530

    change port

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ app.get('/',(req,res)=>{
     res.send({hi:"good to see you"})
 })
 
-const PORT= process.env.PORT || 5000
-app.listen(PORT)
+//const PORT= process.env.PORT || 5000
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
 
 
 
