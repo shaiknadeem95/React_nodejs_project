f9d5f0988494fe851ba32169d791ed0ad20df818
Nadeem Shaik <shaiknadeem98@gmail.com>
2020-11-14 16:56:22 +0530

    heroku_testing

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.get("/",(req,res)=>{
 })
 
 //const PORT= process.env.PORT || 5000
-app.listen(process.env.PORT || 3000, function(){
+app.listen(process.env.PORT || 5000, function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
   });
 
